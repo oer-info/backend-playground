b9247b208edde137ffedd9f417179302e42193a9
CSNWEB <christoph@kiron.ngo>
2020-03-22 11:34:37 +0100

    Log environment variables

--- functions/test.js
+++ functions/test.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
   console.log("Hello from the console");
   const { identity, user } = context.clientContext;
   console.log("Check auth");
-  console.log(event, identity, user);
+  console.log(event, identity, user, process.env);
   callback(null, {
     statusCode: 200,
     body: "No worries, all is working fine!"
