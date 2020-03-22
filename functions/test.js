2a48fcdf556258965a71a6e474228ef6666a055c
CSNWEB <christoph@kiron.ngo>
2020-03-22 11:42:28 +0100

    Try to add test entry to algolia from

--- functions/test.js
+++ functions/test.js
@@ -2,9 +2,30 @@ exports.handler = (event, context, callback) => {
   console.log("Hello from the console");
   const { identity, user } = context.clientContext;
   console.log("Check auth");
-  console.log(event, identity, user, process.env);
+  console.log(identity, user);
   callback(null, {
     statusCode: 200,
     body: "No worries, all is working fine!"
   });
+
+  const algoliasearch = require("algoliasearch");
+
+  const client = algoliasearch("8U7CL41ANW", process.env.ALGOLIA_ADMIN_KEY);
+  const index = client.initIndex("offers");
+
+  const objects = [
+    {
+      objectID: 1,
+      title: "Added by netlify function"
+    }
+  ];
+
+  index
+    .saveObjects(objects)
+    .then(({ objectIDs }) => {
+      console.log(objectIDs);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
