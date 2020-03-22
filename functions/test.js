25dd67827ddfd7ff20dcc792e6e78503b0d74bd2
CSNWEB <christoph@kiron.ngo>
2020-03-22 10:57:55 +0100

    Add identity to test function

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,8 @@
 exports.handler = (event, context, callback) => {
   console.log("Hello from the console");
+  const { identity, user } = context.clientContext;
+  console.log("Check auth");
+  console.log(identity, user);
   callback(null, {
     statusCode: 200,
     body: "No worries, all is working fine!"
