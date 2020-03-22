4c136e03603a8e7261c7ece1b1f31af125c0ff71
CSNWEB <christoph@kiron.ngo>
2020-03-22 11:06:54 +0100

    Add event to log

--- functions/test.js
+++ functions/test.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
   console.log("Hello from the console");
   const { identity, user } = context.clientContext;
   console.log("Check auth");
-  console.log(identity, user);
+  console.log(event, identity, user);
   callback(null, {
     statusCode: 200,
     body: "No worries, all is working fine!"
